3ab4a7edf27e560cd04ea7098a949649018b636e
Eugene Chereshnev <eugenechereshnev@gmail.com>
2016-08-13 13:18:38 -0400

    Change pin version: r71313 -> r64254

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ tar -zxvf NPB3.3.1.tar.gz
 ln -s NPB3.3.1 npb
 cp make.def npb/NPB3.3-OMP/config/
 
-pin_version=2.14-71313-gcc.4.4.7-linux
+pin_version=2.14-67254-gcc.4.4.7-linux
 wget http://software.intel.com/sites/landingpage/pintool/downloads/pin-${pin_version}.tar.gz
 tar -zxvf pin-${pin_version}.tar.gz
 ln -s pin-${pin_version} pin
